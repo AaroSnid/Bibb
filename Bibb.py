073992d027e40a09b5cc41685fbf6a2a61ba91de
AaroSnid <aaronprodouctions@gmail.com>
2025-04-03 23:29:35 -0400

    Update Bibb.py
    Created functions and class definitions

--- Bibb.py
+++ Bibb.py
@@ -1,1 +1,38 @@
+import win32gui
+import time
 
+#This is to designate different "tasks" or study topics. Defininately adding more variables and methods.
+class Event:
+
+    def __init()__(self, name, blacklist):
+        self.name = name
+        self.blacklist = blacklist
+
+    def set_time_goal(goal, duration):
+        self.goal_time = goal
+        self.goal_period = duration
+
+    def update_blacklist(item, state)
+        if "remove" in state:
+            blacklist.remove(item)
+        else:
+            blacklist.add(item)
+
+#This will be ran whenever a session is begun
+def event_loop(Event):
+    initial_time = time.time()
+
+    #GUI Stuff probably
+
+    while():
+        window = Event.get_window_name()
+        if window in Event.blacklist:
+            #function to handle reminder
+
+    time_spent = time.time() - initial_time
+    
+
+#Returns the name of the window that the user is on
+def get_window_name():
+    window = win32gui.GetForegroundWindow()
+    return win32gui.GetWindowText(window)
